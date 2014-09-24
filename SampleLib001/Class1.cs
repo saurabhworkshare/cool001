9e3effbdfd4ae3c5a3ce02a69516df91da9af57b
saurabhworkshare <saurabh.dasgupta@workshare.com>
2014-09-24 16:00:44 +0200

    test commit

--- SampleLib001/Class1.cs
+++ SampleLib001/Class1.cs
@@ -5,7 +5,11 @@ using System.Text;
 
 namespace SampleLib001
 {
+    /// <summary>
+    /// Dummy class
+    /// </summary>
     public class Class1
     {
+        public int MyProperty { get; set; }
     }
 }
